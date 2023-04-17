bf5b02e477b39ef79f07e6f52a035434a541fe56
Gonzalo Ariel Gomez <gonza12ariel@gmail.com>
2023-04-17 16:54:47 -0300

    ejercicio 5 terminado.

--- trabajo_practico_1/ejercicio_5/Test.java
+++ trabajo_practico_1/ejercicio_5/Test.java
@@ -1,7 +1,7 @@
 package ejercicio_5;
 
 import ejercicio_1.Node;
-import ejercicio_4.LinkedList;
+
 
 public class Test {
 
@@ -12,7 +12,7 @@ public class Test {
 		lista1.insertFront("testeo 2");
 		lista1.insertFront("testeo 3");
 		lista1.insertFront("testeo 4");
-		for(int i:lista1) {
+		for(String i:lista1) {
 			System.out.println(i);
 		}
 	}
